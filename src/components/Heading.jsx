2d6bf8213c3c2b4118519bf0d3005c23c2a6101a
Bradley Mubenga <bradleymubenga777@gmail.com>
2021-05-26 15:34:56 +0200

    Netlify Bug Fixes Final!

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -5,6 +5,7 @@ export default function Heading() {
   return (
     <header className="App-header">
       <img
+        alt="this is the logo, are you happy?"
         className="logoImg"
         src="https://image.flaticon.com/icons/svg/139/139899.svg"
       />
